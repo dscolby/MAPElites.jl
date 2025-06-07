2b8fb0594cebd8f60089dbdaf2f8ae71ffe5f5d9
Darren Colby <dscolby17@gmail.com>
2025-06-07 00:42:12 -0500

    Added JET back

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using JET
         Aqua.test_all(MAPElites)
     end
     @testset "Code linting (JET.jl)" begin
-        JET.test_package(MAPElites; target_defined_modules = true)
+        JET.test_package(MAPElites; target_defined_modules=true)
     end
     # Write your tests here.
 end
